7277a39e57549854c0969ceff823ef741ae74d19
daktari-art <62507374+daktari-art@users.noreply.github.com>
2025-10-28 16:18:11 +0300

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,45 +1,5 @@
-// Background service worker for Glyph Language extension
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('🔮 Glyph Language DevTools extension installed');
-});
+// Background service worker for Glyph Debugger
+console.log('🔮 Glyph Debugger background service worker loaded');
 
-chrome.runtime.onConnect.addListener((port) => {
-    if (port.name === "glyph-devtools") {
-        console.log('🔮 Glyph DevTools connected');
-        
-        port.onMessage.addListener((message) => {
-            switch (message.type) {
-                case 'START_TRACING':
-                    startTracing(port);
-                    break;
-                case 'STOP_TRACING':
-                    stopTracing(port);
-                    break;
-            }
-        });
-    }
-});
-
-let isTracing = false;
-
-function startTracing(port) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0]) {
-            isTracing = true;
-            port.postMessage({ 
-                type: 'TRACING_STATE_CHANGED', 
-                isTracing: true 
-            });
-            console.log('🔮 Tracing started');
-        }
-    });
-}
-
-function stopTracing(port) {
-    isTracing = false;
-    port.postMessage({ 
-        type: 'TRACING_STATE_CHANGED', 
-        isTracing: false 
-    });
-    console.log('🔮 Tracing stopped');
-}
+// No complex background logic needed for basic functionality
+// The content script and DevTools panel communicate directly via window.postMessage
